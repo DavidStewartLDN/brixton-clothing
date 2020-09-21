fafd812b374cb4ceaa523c49de4898aa15a9368d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-21 12:37:28 +0100

    Added React Router dom for navigation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import HomePage from './pages/homepage/homepage.component';
+
+
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Router>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </Switch>
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
