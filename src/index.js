11e2895d72e88a7b21ab13734b4e064f5a2ea8c3
DavidStewartLDN <davidstewart.work@gmail.com>
2020-09-28 12:22:10 +0100

    Moved Switch out of index.js to App.js
    Corrected link path for HatsPage Route

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import HomePage from './pages/homepage/homepage.component';
-
-
-
-const HatsPage = (props) => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-);
 
 
 ReactDOM.render(
-  <React.StrictMode>
   <Router>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='shop/hats' component={HatsPage} />
-    </Switch>
-  </Router>
-  </React.StrictMode>,
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
