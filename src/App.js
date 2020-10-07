4cdfac2bbbbd30bb957521dc483493c1330cd794
David Stewart <davidstewart.work@gmail.com>
2020-10-07 21:14:07 +0100

    Implemented userReducer in app component

--- src/App.js
+++ src/App.js
@@ -10,37 +10,33 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if ( userAuth ) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
+          setCurrentUser(
             {
-              currentUser: {
                 id: snapShot.id,
                 ...snapShot.data()
-              }
           },
           () => {
             console.log(this.state);
           })
         });
       } else {
-        this.setState({ currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
 
     });
@@ -64,4 +60,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps)(App);
